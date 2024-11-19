c983bd0750f26e8b4369033814ed9921c176f068
Chad1101 <68296120+draking1101@users.noreply.github.com>
2024-11-19 21:24:57 +0800

    調整config

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://draking1101.github.io/Blog/',
-  base: '/',
+  site: 'https://draking1101.github.io',
+  base: '/Blog',
   trailingSlash: 'always',
   integrations: [
     tailwind(),
