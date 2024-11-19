830531d29aeddeae4fcb9bccec9e157c1a2fc056
Chad1101 <68296120+draking1101@users.noreply.github.com>
2024-11-19 21:00:21 +0800

    Initial file

---
+++ src/config.ts
@@ -0,0 +1,77 @@
+import type {
+  LicenseConfig,
+  NavBarConfig,
+  ProfileConfig,
+  SiteConfig,
+} from './types/config'
+import { LinkPreset } from './types/config'
+
+export const siteConfig: SiteConfig = {
+  title: '龍嘯天',
+  subtitle: '人生紀錄',
+  lang: 'zh_TW',         // Support : 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  themeColor: {
+    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false,     // Hide the theme color picker for visitors
+  },
+  banner: {
+    enable: true,
+    // Default src: 'assets/images/demo-banner.png'
+    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    credit: {
+      enable: false,         // Display the credit text of the banner image
+      text: '',              // Credit text to be displayed
+      url: ''                // (Optional) URL link to the original artwork or artist's page
+    }
+  },
+  toc: {
+    enable: true,           // Display the table of contents on the right side of the post
+    depth: 2                // Maximum heading depth to show in the table, from 1 to 3
+  },
+  favicon: [    // Leave this array empty to use the default favicon
+    // {
+    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    // }
+  ]
+}
+
+export const navBarConfig: NavBarConfig = {
+  links: [
+    LinkPreset.Home,
+    LinkPreset.Archive,
+    LinkPreset.About,
+    {
+      name: 'GitHub',
+      url: 'https://github.com/draking1101',     // Internal links should not include the base path, as it is automatically added
+      external: true,                               // Show an external link icon and will open in a new tab
+    },
+  ],
+}
+
+
+// 左半邊個人檔案設定
+export const profileConfig: ProfileConfig = {
+  // Default avatar: 'assets/images/demo-avatar.png'
+  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: '龍嘯天',
+  bio: '覺得寫程式很帥而開始學習Coding，但沒這方面天分的普通人',
+  links: [
+    // Visit https://icones.js.org/ for icon codes
+    // You will need to install the corresponding icon set if it's not already included
+    // `pnpm add @iconify-json/<icon-set-name>`
+    {
+      name: 'GitHub',
+      icon: 'fa6-brands:github',
+      url: 'https://github.com/draking1101',
+    },
+  ],
+}
+
+export const licenseConfig: LicenseConfig = {
+  enable: true,
+  name: 'CC BY-NC-SA 4.0',
+  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+}
---
+++ src/content/spec/about.md
@@ -0,0 +1,8 @@
+# About
+## 作品集
+這是我替朋友架設的網站 [xiaojean0128.com](https://xiaojean0128.com/)，同時也是第一個網站作品。底下有github連結。
+
+::github{repo="draking1101/xiaojean0128"}
+
+## 網站簡介
+這個網站會用來記錄我的人生經歷以及所學技能
